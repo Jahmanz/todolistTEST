cecc8168a994ac7cc970e02952bdca729550d3da
Guest User <Guest@epicodus-118.local>
2018-02-08 10:46:20 -0800

    adss saving and finding elemetns and restful routing

--- Controller/ItemsController.cs
+++ Controller/ItemsController.cs
@@ -7,23 +7,29 @@ namespace ToDoList.Controllers
     public class ItemsController : Controller
     {
 
-        [HttpGet("/items")]
-        public ActionResult Index()
+      [HttpGet("/items")]
+      public ActionResult Index()
+      {
+          List<Item> allItems = Item.GetAll();
+          return View(allItems);
+      }
+
+      [HttpGet("/items/new")]
+      public ActionResult CreateForm()
+      {
+          return View();
+      }
+      [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
         {
-            List<Item> allItems = Item.GetAll();
-            return View(allItems);
+            Item item = Item.Find(id);
+            return View(item);
         }
 
-        [HttpGet("/items/new")]
-        public ActionResult CreateForm()
-        {
-            return View();
-        }
-        [HttpPost("/items")]
-        public ActionResult Create()
-        {
-          Item newItem = new Item (Request.Form["new-item"]);
-          newItem.Save();
+      [HttpPost("/items")]
+      public ActionResult Create()
+      {
+          Item newItem = new Item(Request.Form["new-item"]);
           List<Item> allItems = Item.GetAll();
           return View("Index", allItems);
         }
